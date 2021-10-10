c2233cd88e124bdcb6bf44af1c7c2483a0f8d97c
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-10-09 20:43:26 -0500

    Más cambios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 let toggleStatus = false
-
 const toggleNav = function() {
     const getMenu = document.getElementById('menu-mobile')
 
@@ -11,3 +10,20 @@ const toggleNav = function() {
         toggleStatus = false
     }
 }
+
+
+const shrinkNavbar = () => {
+    const header = document.getElementById('header')
+    const logo = document.getElementById('logo')
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        header.classList.add('shrink')
+        logo.classList.add('small')
+        logo.classList.remove('normal')
+    } else {
+        header.classList.remove('shrink')
+        logo.classList.remove('small')
+        logo.classList.add('normal')
+    }
+}
+
+window.onscroll = () => shrinkNavbar()
